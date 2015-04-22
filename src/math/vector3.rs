d5baf4e7701cf76417d8cf78d36e104918134868
identifer <sorrow722@gmail.com>
2015-04-22 20:59:18 +0800

    module vector3

--- src/math/vector3.rs
+++ src/math/vector3.rs
@@ -1,5 +1,5 @@
 use std::cmp::{PartialEq, Eq};
-use std::ops::{Add, Sub, Mul};
+use std::ops::{Neg, Add, Sub, Mul, Div};
 
 #[derive(Copy, Debug)]
 pub struct Vector3 {
@@ -27,6 +27,14 @@ impl Vector3 {
         self.y *= reciprocal_length;
         self.z *= reciprocal_length;
     }
+
+    pub fn cross(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
 }
 
 impl Clone for Vector3 {
@@ -35,6 +43,18 @@ impl Clone for Vector3 {
             x: self.x,
             y: self.y,
             z: self.z,
+        }
+    }
+}
+
+impl Neg for Vector3 {
+    type Output = Vector3;
+
+    fn neg(self) -> Vector3 {
+        Vector3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
         }
     }
 }
@@ -95,6 +115,18 @@ impl Mul for Vector3 {
     }
 }
 
+impl Div<f64> for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, other: f64) -> Vector3 {
+        Vector3 {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        }
+    }
+}
+
 impl PartialEq for Vector3 {
     fn eq(&self, other: &Vector3) -> bool {
         self.x == other.x && self.y == other.y && self.z == other.z
@@ -127,6 +159,28 @@ mod test {
     }
 
     #[test]
+    fn test_cross() {
+        let i = Vector3::new(1.0, 0.0, 0.0);
+        let j = Vector3::new(0.0, 1.0, 0.0);
+        let k = Vector3::new(0.0, 0.0, 1.0);
+
+        assert_eq!(i.cross(&j), k);
+        assert_eq!(j.cross(&i), -k);
+
+        assert_eq!(j.cross(&k), i);
+        assert_eq!(k.cross(&j), -i);
+
+        assert_eq!(k.cross(&i), j);
+        assert_eq!(i.cross(&k), -j);
+    }
+
+    #[test]
+    fn test_neg() {
+        let v = Vector3::new(1.0, 2.0, 3.0);
+        assert_eq!(-v, Vector3::new(-1.0, -2.0, -3.0));
+    }
+
+    #[test]
     fn test_add() {
         let v1 = Vector3::new(1.0, 2.0, 3.0);
         let v2 = Vector3::new(4.0, 5.0, 6.0);
@@ -150,6 +204,12 @@ mod test {
         assert_eq!(v1 * v2, 32.0);
         assert_eq!(2.0 * v1, v3);
         assert_eq!(v1 * 2.0, v3);
+    }
+
+    #[test]
+    fn test_div() {
+        let v = Vector3::new(2.0, 4.0, 6.0);
+        assert_eq!(v / 2.0, Vector3::new(1.0, 2.0, 3.0));
     }
 
 }
